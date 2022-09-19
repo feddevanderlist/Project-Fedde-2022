0fa54e7ebd5c155e99886500e3143059095b3752
vandalist <feddevanderlist@gmail.com>
2022-09-19 12:32:33 +0200

    added some documentation and sql

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Hoe start je het project?
+
+- install php 8.1
+- install composer
+- install symfony cli om je leven makkelijker te maken
+- run `composer install`
+
+Vervang deze regel in de
+.env `DATABASE_URL="mysql://username:password@127.0.0.1:3306/project-fedde-2020?serverVersion=8&charset=utf8mb4"`
+
+- voer deze functie uit `symfony server:start`
+
+Nu zou de webserver gestart moeten zijn.
+
+- In de migrations folder staan de sql import scripts zodat je gelijk wat data hebt om mee te werken.
---
+++ migrations/merk.sql
@@ -0,0 +1,31 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+INSERT INTO `merk` (`id`, `naam`, `land_opgericht`) VALUES
+(1, 'BMW', 'Duitsland'),
+(2, 'Peugot', 'Frankrijk'),
+(3, 'Alfa Romeo', 'Italie'),
+(4, 'Abarth', 'Turijn'),
+(5, 'Aston Martin', 'Engeland'),
+(6, 'Audi', 'Duitsland'),
+(7, 'Bentley', 'Engeland'),
+(8, 'Bugatti', 'Frankrijk'),
+(9, 'Citroën', 'Frankrijk'),
+(10, 'Ford', 'Verenigde Staten'),
+(11, 'Hyundai', 'Zuid-Korea'),
+(12, 'Mercedes Benz', 'Duitsland'),
+(13, 'Mitsubishi', 'Japan'),
+(14, 'Nissan', 'Japan'),
+(15, 'Porsche', 'Duitsland'),
+(16, 'Seat', 'Spanje'),
+(17, 'Saab', 'Zweden');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
