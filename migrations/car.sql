c89f76f92011d76a9d8d3fc67c23b8f455bb549a
vandalist <feddevanderlist@gmail.com>
2022-09-29 09:06:38 +0200

    fixed insert files

--- migrations/car.sql
+++ migrations/car.sql
@@ -1,14 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 23, 2022 at 03:57 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,16 +8,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `project-fedde-2020`
---
-
--- --------------------------------------------------------
-
-
---
--- Dumping data for table `car`
---
 
 INSERT INTO `car` (`id`, `merk_id`, `motor_id`, `kleur`, `gewicht`, `brandstof`, `top_snel_heid`, `bouwjaar`, `automaat`, `aantal_versnellingen`, `aantal_deuren`, `prijs`, `naam`) VALUES
 (1, 10, 1, 'grijs', 1500, 'Benzine', 202, '2018-09-01', 0, 6, 5, 40000, 'C-Max'),
@@ -34,10 +15,6 @@ INSERT INTO `car` (`id`, `merk_id`, `motor_id`, `kleur`, `gewicht`, `brandstof`,
 (3, 10, 1, 'Zilver', 1000, 'Benzine', 196, '2021-09-01', 1, 7, 3, 30000, 'Fiesta'),
 (4, 8, 2, 'Blauw', 1600, 'Benzine', 420, '2017-09-21', 1, 7, 2, 2400000, 'Bugatti Chiron'),
 (5, 8, 2, 'Rood', 1550, 'Benzine', 420, '2017-09-21', 1, 7, 2, 2400000, 'Chiron Sport');
-
---
--- Indexes for dumped tables
---
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- migrations/motor.sql
+++ migrations/motor.sql
@@ -1,41 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 23, 2022 at 03:58 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `project-fedde-2020`
---
-
--- --------------------------------------------------------
---
--- Dumping data for table `motor`
---
 
 INSERT INTO `motor` (`id`, `naam`, `vermogen`, `aantal_cylinders`) VALUES
 (1, '1.0 Ecoboost', 125, 3),
 (2, '8L Turbo-Charged', 1103, 16),
 (3, '1.6 L N20 I4', 168, 4);
-
---
--- Indexes for dumped tables
---
-
-
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
